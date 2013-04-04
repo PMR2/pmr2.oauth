8ab0c41c5d67a505cb2a842044a8ce96b5eff2de
Tommy Yu <tommy.yu@auckland.ac.nz>
2013-04-04 19:56:36 +1300

    Now depend on oauthlib 0.4.0

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(name='pmr2.oauth',
       install_requires=[
           'setuptools',
           # -*- Extra requirements: -*-
-          'oauthlib>=0.3.8,<0.4',
+          'oauthlib==0.4.0',
           'zope.testing',
           'zope.component',
           'zope.interface',
