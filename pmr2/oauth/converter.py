749fcdf191b160f05e52273521287bef8b91d19e
Tommy Yu <tommy.yu@auckland.ac.nz>
2013-10-21 18:55:33 +1300

    Change converter to not return inner NO_VALUE
    - No more erroring out on new errors.
    - Fixes issue #3

--- pmr2/oauth/converter.py
+++ pmr2/oauth/converter.py
@@ -1,5 +1,6 @@
 import zope.component
 
+from z3c.form.interfaces import NO_VALUE
 from z3c.form.interfaces import IObjectWidget
 from z3c.form.object import ObjectConverter
 
@@ -13,6 +14,18 @@ class SchemaMethodObjectConverter(ObjectConverter):
     """
 
     zope.component.adapts(ISchemaMethodObject, IObjectWidget)
+
+    def toWidgetValue(self, value):
+        raw = super(SchemaMethodObjectConverter, self).toWidgetValue(value)
+        if raw is NO_VALUE:
+            return raw
+        result = {}
+        for k, v in raw.iteritems():
+            if v is NO_VALUE:
+                # NO_VALUE is not known to be not iterable, but None is.
+                v = None
+            result[k] = v
+        return result
 
     def toFieldValue(self, value):
         # The value provided is a dict, and this isn't an for an object
